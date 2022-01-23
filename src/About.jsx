f1315ba12eafd55d64e3e76c99edc12df9baa0a1
Tanakorn Phongsing <t.phongsing@gmail.com>
2022-01-23 21:50:26 +0700

    update iframe title

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <MainLayout>
         <DocumentTitle title='เกี่ยวกับเรา' />
 
-        <iframe width={'100%'} height={600} src="https://www.youtube.com/embed/SwvAGEGRcoY" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <iframe title="This is a youtube"  width={'100%'} height={600} src="https://www.youtube.com/embed/SwvAGEGRcoY" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 
         <div className="container mb-5">
           <h1 className="my-4">เกี่ยวกับเรา</h1>
