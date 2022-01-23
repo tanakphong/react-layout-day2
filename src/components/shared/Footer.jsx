53ed994925dfcd8e141b43da1d531e43a1f13b72
Tanakorn Phongsing <t.phongsing@gmail.com>
2022-01-23 21:54:11 +0700

    update footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,176 +1,172 @@
 function Footer() {
   return (
-    <section className="footer mt-3">
-      <div className="container">
-        <footer className="py-5">
-          <div className="row">
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Menu 1</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    FAQs
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    About
-                  </a>
-                </li>
-              </ul>
+<section classname="footer mt-3">
+  <div classname="container">
+    <footer classname="py-5">
+      <div classname="row">
+        <div classname="col-6 col-md-2 mb-3">
+          <h5>Menu 1</h5>
+          <ul classname="nav flex-column">
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Home
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Features
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Pricing
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div classname="col-6 col-md-2 mb-3">
+          <h5>Menu 2</h5>
+          <ul classname="nav flex-column">
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Home
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Features
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Pricing
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div classname="col-6 col-md-2 mb-3">
+          <h5>Menu 3</h5>
+          <ul classname="nav flex-column">
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Home
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Features
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Pricing
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div classname="col-6 col-md-2 mb-3">
+          <h5>Menu 4</h5>
+          <ul classname="nav flex-column">
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Home
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Features
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                Pricing
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                FAQs
+              </a>
+            </li>
+            <li classname="nav-item mb-2">
+              <a href="#" classname="nav-link p-0 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div classname="col-md-4">
+          <form>
+            <h5>Subscribe to our newsletter</h5>
+            <p>Monthly digest of whats new and exciting from us.</p>
+            <div classname="d-flex w-100 gap-2">
+              <input id="newsletter1" type="text" classname="form-control" placeholder="Email address" />
+              <button classname="btn btn-primary" type="button">
+                Subscribe
+              </button>
             </div>
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Menu 2</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    FAQs
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Menu 3</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    FAQs
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6 col-md-2 mb-3">
-              <h5>Menu 4</h5>
-              <ul className="nav flex-column">
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    FAQs
-                  </a>
-                </li>
-                <li className="nav-item mb-2">
-                  <a href="#" className="nav-link p-0 text-muted">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <form>
-                <h5>Subscribe to our newsletter</h5>
-                <p>Monthly digest of whats new and exciting from us.</p>
-                <div className="d-flex w-100 gap-2">
-                  <input
-                    id="newsletter1"
-                    type="text"
-                    className="form-control"
-                    placeholder="Email address"
-                  />
-                  <button className="btn btn-primary" type="button">
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="d-flex justify-content-between py-4 my-4 border-top">
-            <p>© 2021 Company, Inc. All rights reserved.</p>
-            <ul className="list-unstyled d-flex">
-              <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width={24} height={24}>
-                    <use xlinkHref="#twitter" />
-                  </svg>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width={24} height={24}>
-                    <use xlinkHref="#instagram" />
-                  </svg>
-                </a>
-              </li>
-              <li className="ms-3">
-                <a className="link-dark" href="#">
-                  <svg className="bi" width={24} height={24}>
-                    <use xlinkHref="#facebook" />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </footer>
+          </form>
+        </div>
       </div>
-    </section>
+      <div classname="d-flex justify-content-between py-4 my-4 border-top">
+        <p>© 2021 Company, Inc. All rights reserved.</p>
+        <ul classname="list-unstyled d-flex">
+          <li classname="ms-3">
+            <a classname="link-dark" href="#">
+              <svg classname="bi" width="{24}" height="{24}">
+                <use xlinkhref="#twitter" />
+              </svg>
+            </a>
+          </li>
+          <li classname="ms-3">
+            <a classname="link-dark" href="#">
+              <svg classname="bi" width="{24}" height="{24}">
+                <use xlinkhref="#instagram" />
+              </svg>
+            </a>
+          </li>
+          <li classname="ms-3">
+            <a classname="link-dark" href="#">
+              <svg classname="bi" width="{24}" height="{24}">
+                <use xlinkhref="#facebook" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+</section>
+
   );
 }
 
